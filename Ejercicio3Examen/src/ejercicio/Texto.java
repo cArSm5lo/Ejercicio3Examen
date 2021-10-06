d8639e6f6353ba28b54d9085c628be7d62df36ec
carme <carme@LAPTOP-HHGLH0KG>
2021-10-06 18:26:06 +0200

    Commit realizado.

---
+++ Ejercicio3Examen/src/ejercicio/Texto.java
@@ -0,0 +1,12 @@
+package ejercicio;
+
+public class Texto {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Buenas tardes.");
+		System.out.println("Que tenga un buen día.");
+
+	}
+
+}
